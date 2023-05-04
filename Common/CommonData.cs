9b2b9ae39b7b21dc570b761e66ffe06ff887edb8
AP\Mayur.More <mayur.more@walters.kluwer.com>
2023-05-04 18:37:50 +0530

    Added TimeToMerge API

---
+++ Common/CommonData.cs
@@ -0,0 +1,26 @@
+﻿namespace Elite_CG_API.Common
+{
+    public class CommonData
+    {
+        private static IConfiguration AppSetting = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        public static string Base_URL = AppSetting["CommonFields:Base_URL"];
+        public static string Repo_Owner = AppSetting["CommonFields:Repo_Owner"];
+        public static string Repo_Name = AppSetting["CommonFields:Repo_Name"];
+        public static string Token = AppSetting["CommonFields:Token"];
+
+        public HttpClient Client { get; set; }
+
+        public CommonData() {
+            Client = new HttpClient();
+            Client.DefaultRequestHeaders.Add("Accept", "application/vnd.github+json");
+            Client.DefaultRequestHeaders.Add("Authorization", "Bearer " + CommonData.Token);
+            Client.DefaultRequestHeaders.Add("X-GitHub-Api-Version", "2022-11-28");
+            Client.DefaultRequestHeaders.Add("User-Agent", CommonData.Repo_Name);
+        }
+
+    }
+}
---
+++ Models/PullRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace Elite_CG_API.Models
+{
+    public class PullRequest
+    {
+        public string? id { get; set; }
+        public string? title { get; set; }
+        public DateTime? created { get; set; }
+        public DateTime? updated { get; set; }
+        public DateTime? merged { get; set; }
+        public DateTime? closed { get; set; }
+        public string? author { get; set; }
+        public string? url { get; set; }
+        public double? totalPRMergedHrs { get; set; }
+        public double? totalPRMergedMinutes { get; set; }
+        public double? totalPRMergedDays { get; set; }
+    }
+}
