a0686e2ca2f985d1dfc79c75fffc328b1ebd1e09
Darpan Mistry <Darpan.Mistry@wolterskluwer.com>
2023-05-05 14:54:30 +0530

    Deserialize CommitCount Object

--- DataAccess/CommitCountProvider.cs
+++ DataAccess/CommitCountProvider.cs
@@ -11,15 +11,15 @@ namespace Elite_CG_API.DataAccess
         {
         }
 
-        internal async Task<Rootobject> GetCommitCount()
+        internal async Task<IEnumerable<CommitCount>> GetCommitCount()
         {
-            Rootobject rootobject = new Rootobject();
-            using (var response = await new CommonData().Client.GetAsync(CommonData.Base_URL + "repos/"+CommonData.Repo_Owner + "/" + CommonData.Repo_Name + "/"+"commits"))
+            IEnumerable<CommitCount> result= new List<CommitCount>();
+            using (var response = await new CommonData().Client.GetAsync(CommonData.Base_URL + "repos/"+CommonData.Repo_Owner + "/" + CommonData.Repo_Name + "/"+ "commits?since=2023-04-26T10:50:00z&until=2023-05-05T10:59:00z"))
             {
                 string apiResponse = await response.Content.ReadAsStringAsync();
-                rootobject = JsonConvert.DeserializeObject<Rootobject>(apiResponse);
+                result = JsonConvert.DeserializeObject<IEnumerable<CommitCount>>(apiResponse);
             }
-            return rootobject;
+            return result ;
         }
 
     }
